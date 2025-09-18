1ada7d9fbbe45f55966d98b1daf7acb2df3f0871
mahatav6 <mahatabcall56@gmail.com>
2025-09-18 14:11:45 +0600

    typeScript learning

---
+++ array.ts
@@ -0,0 +1,13 @@
+let userName: string[];
+
+userName = ["toma", "kona", "habiba", "mahatab"]
+
+console.log(userName[2])
+
+
+let multiType: (string | number | boolean )[];
+
+multiType = ["hello", 1 , "mahatab", true, 5]
+
+multiType.sort();
+console.log(multiType)
---
+++ index.ts
@@ -0,0 +1,10 @@
+
+type User = {
+    id: number;
+    name: string;
+    isAdmin: boolean;
+}
+
+const user: User = { id: 1 , name: "mahatab", isAdmin: true }
+
+console.log(user)
